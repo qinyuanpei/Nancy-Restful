dd59009a2e15546dc8b0b4f62a1d13f1dae88f88
PayneQin <qinyuanpei@chinasofti.com>
2018-04-11 17:39:54 +0800

    更新脚本

---
+++ AspNet.Owin.Jwt/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.Owin.Security.Cookies;
+
+
+[assembly: OwinStartup(typeof(AspNet.Owin.Jwt.Startup))]
+
+namespace AspNet.Owin.Jwt
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var opts = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                Provider = new OAuthAuthorizationServerProvider(),
+                AuthenticationMode = AuthenticationMode.Active,
+                TokenEndpointPath = new PathString("/oauth/token"),
+                AuthorizeEndpointPath = new PathString("/oauth/authorize"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
+
+            };
+
+            app.UseOAuthAuthorizationServer(opts);
+        }
+    }
+}
